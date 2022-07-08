bda63d186ebea3e3ebc7e1cfa3f1c5fa78d4906d
Markus Holmström (MawKKe) <markus@mawkke.fi>
2022-07-08 17:57:29 +0000

    Add module documentation

--- main.go
+++ main.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Configure a CMake project automatically using .vscode/settings.json
 package main
 
 import (
