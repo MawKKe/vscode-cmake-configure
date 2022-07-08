6f550ff5de68f4dc9a225e003b26547282d2d355
Markus Holmström (MawKKe) <markus@mawkke.fi>
2022-07-08 13:11:05 +0000

    Add main functionality - a working program

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/MawKke/vscode-cmake-configure
 
 go 1.16
+
+require (
+	github.com/alessio/shellescape v1.4.1 // indirect
+	github.com/tidwall/jsonc v0.3.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,95 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"os/exec"
+	"sort"
+
+	"github.com/alessio/shellescape"
+	"github.com/tidwall/jsonc"
 )
 
-func main(){
-	fmt.Println("Hello")
+type Settings struct {
+	CMakeConfigureSettings map[string]string `json:"cmake.configureSettings"`
+}
+
+func ReadSettings(input string) (Settings, error) {
+	s, err := os.ReadFile(input)
+
+	if err != nil {
+		return Settings{}, err
+	}
+
+	var settings Settings
+	err = json.Unmarshal(jsonc.ToJSON(s), &settings)
+	if err != nil {
+		return Settings{}, err
+	}
+	return settings, nil
+}
+
+func (s Settings) ExtractCMakeConfigureSettingCommandline() []string {
+	var args []string
+	for key, value := range s.CMakeConfigureSettings {
+		//fmt.Println(key, value)
+		args = append(args, fmt.Sprintf("-D%s=%v", key, shellescape.Quote(value)))
+	}
+	sort.Strings(args)
+	return args
+}
+
+func ShowCommand(args []string) {
+	fmt.Printf("%v\n", args)
+
+	cmd := exec.Command("cmake", args...)
+	res := cmd.Run()
+	if res != nil {
+		fmt.Printf("error: %v\n", res)
+	}
+	cmd.Wait()
+	os.Exit(cmd.ProcessState.ExitCode())
+}
+
+func GetEnvOrDefault(key string, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists && value != "" {
+		return value
+	}
+	return fallback
+}
+
+func main() {
+	//inFile := flag.String("i", ".vscode/settings.json", "input file, if not $PWD/.vscode/settings.json")
+	//flag.Parse()
+
+	inFile := GetEnvOrDefault("VSCODE_SETTINGS", ".vscode/settings.json")
+
+	settings, err := ReadSettings(inFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	args := settings.ExtractCMakeConfigureSettingCommandline()
+
+	//fmt.Print(strings.Join(args, " "))
+
+	args = append(args, os.Args[1:]...)
+
+	cmd := exec.Command("cmake", args...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	fmt.Println(cmd.Args)
+
+	res := cmd.Run()
+
+	if res != nil {
+		fmt.Printf("error: %v\n", res)
+	}
+
+	os.Exit(cmd.ProcessState.ExitCode())
 }
